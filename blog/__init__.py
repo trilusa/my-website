7ccc755dbc06c2191fe4df2c789c3161abf87576
trilusa <adriansalustri@gmail.com>
2021-06-03 13:36:08 -0400

    changed to boots strap, modified html's. Completely replaced base.html

--- blog/__init__.py
+++ blog/__init__.py
@@ -7,6 +7,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+bootstrap = Bootstrap(app)
 
 from blog import models, routes, errors
 
