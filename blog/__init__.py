7123791600b5e6e434d14d9071cc295fca45cba5
trilusa <adriansalustri@gmail.com>
2021-06-05 19:06:39 -0400

    added flask-bootstap by chamnging mybase

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 ckeditor = CKEditor(app)
+
+bootstrap = Bootstrap(app)
 
 from blog import routes, models
